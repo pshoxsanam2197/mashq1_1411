4d241c035df1c95e449fea68e34ac505d2464951
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2025-11-14 15:26:10 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+ # 1
+matn = "     Python dasturlash     "
+print(matn)
+print(matn.lstrip())
+
+# 2
+ matn = "hello world"
+print(matn)
+print(matn.upper())
+print(len(matn))
+
+# 3
+matn = "  SALOM DUNYO 123  "
+print(matn)
+print(matn.lower())
+print(matn.strip())
+
+# 4
+m = "     o'zbekiston     "
+print(m)
+print(m.rsplit())
+print(m.capitalize())
+
+# 5
+m = 'python3.11'
+print(m)
+print(m.title())
+print(len(m)*2)
+
+# 6
+m = '   Toshkent 2025   '
+print(m)
+print(m.strip())
+print(m.upper())
+
+# 7
+n = 'ALGORITIM VA DASTURLASH  '
+print(n)
+print(n.lower())
+print(n.lstrip())
